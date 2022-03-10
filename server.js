93d444a77cdd6f681688e66cc42014bd6fee5f97
joannewei <joannewei0921@gmail.com>
2022-03-11 01:15:51 +0800

    新增 todo POST API

--- server.js
+++ server.js
@@ -8,35 +8,50 @@ const requestListener = (req, res) => {
     'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
     'Content-Type': 'application/json'
   }
-  
-  console.log(req.url);
+
+  let body = "";
+  req.on("data", chunk => {
+    body += chunk
+  })
+
+
   if (req.url == "/todos" && req.method == "GET") {
     res.writeHead(200, headers);
     res.write(JSON.stringify({
-      "status":"success",
+      "status": "success",
       "data": todos
     }));
-    console.log("getData");
-  }else if(req.url =="/todos" && req.method =="POST"){
-    res.writeHead(200, headers);
-    res.write(JSON.stringify({
-      "status": "success",
-      "data": {
-        "123":"123"
-      }
-    }));
-    console.log("postData");
+    res.end()
+  } else if (req.url == "/todos" && req.method == "POST") {
+
+    req.on("end", () => {
+      res.writeHead(200, headers);
+      const title = JSON.parse(body).title
+      console.log(title);
+      todos.push({
+        "title": title,
+        "id": uuidv4()
+      })
+      res.write(JSON.stringify({
+        "status": "success",
+        "data": todos
+      }));
+      res.end()
+    });
+
   } else if (req.method == "OPTIONS") {
     res.writeHead("200", headers)
+    res.end()
   } else {
     res.writeHead(404, headers)
     res.write(JSON.stringify({
       "status": "fail",
-      "message":"error!!!"
+      "message": "error!!!"
     }))
+    res.end()
   }
 
-  res.end()
+  // res.end()
 }
 const server = http.createServer(requestListener)
 server.listen(3005)
